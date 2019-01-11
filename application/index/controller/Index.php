4b787ca1062e8b2113e74d78725484f9edacf463
47 <1072227208@qq.com>
2019-01-11 11:51:34 +0800

    11

---
+++ application/index/controller/Index.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\index\controller;
+
+use think\Container;
+use think\Controller;
+use think\facade\Debug as _debug;
+
+use think\Model;
+class Index extends Model
+{
+    public function index()
+    {
+        $bug=new _debug();
+        $bug->dump(11);
+
+    }
+
+    public function hello($name = 'ThinkPHP5')
+    {
+        return 'hello,' . $name;
+    }
+}
