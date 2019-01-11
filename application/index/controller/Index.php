9ff5dae4e69b59597f030e62af9298cfb580091f
47 <1072227208@qq.com>
2019-01-11 12:35:39 +0800

    Update Index.php

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -17,6 +17,10 @@ class Index extends Model
 
     public function hello($name = 'ThinkPHP5')
     {
+<<<<<<< HEAD
         return 'dsd' . $name;
+=======
+        return 'hello' . $name;
+>>>>>>> parent of 1368df1... Update Index.php
     }
 }
